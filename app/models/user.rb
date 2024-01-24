78a4a7f5ce02b0a96461fae6e7196dc42d192f0f
Rishabh Sairawat <rishabhsairawat01@gmail.com>
2024-01-24 15:41:10 +0530

    Test coderabbit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   encrypts :email, :mobile
   blind_index :email, :mobile
   has_many :rental_units, dependent: :destroy, inverse_of: :user
+
+
+  def print_user_info
+    puts get_user
+  end
+
+  def get_user_info
+    return "User: #{email} #{mobile}"
+  end
 end
